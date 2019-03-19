e5049371b905c1315b12cef14a0bf062aedba31e
Micah Huber <micah.huber@mailbox.org>
2019-03-19 14:22:13 -0400

    Start adding datadog to the controller.

---
+++ pkg/controller/datadog.go
@@ -0,0 +1,9 @@
+package controller
+
+
+import (
+  log "github.com/sirupsen/logrus"
+  "github.com/mjhuber/dd-manager/pkg/util"
+  "github.com/mjhuber/dd-manager/conf"
+  "github.com/zorkian/go-datadog-api"
+)
