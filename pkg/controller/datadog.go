3b873fe2b187befceb7f3250312b8d54a5b115ee
Micah Huber <micah.huber@mailbox.org>
2019-04-01 09:35:00 -0400

    Add datadog client bindings

--- pkg/controller/datadog.go
+++ pkg/controller/datadog.go
@@ -9,15 +9,8 @@ import (
 )
 
 
-
-
-
-
-
-
 func GetProvisionedMonitor(config conf.Config, monitor conf.Monitor) (*datadog.Monitor, error) {
-  client := datadog.NewClient(config.DatadogApiKey, config.DatadogAppKey)
-  monitors, err := client.GetMonitorsByTags([]string{config.OwnerTag})
+  monitors, err := GetProvisionedMonitors(config)
   if err != nil {
     log.Errorf("Error getting monitors: %v", err)
     return nil, err
@@ -25,9 +18,32 @@ func GetProvisionedMonitor(config conf.Config, monitor conf.Monitor) (*datadog.M
 
   for _, ddMonitor := range monitors {
     if *ddMonitor.Name == monitor.Name {
-      // monitor exists - return it
       return &ddMonitor, nil
     }
   }
   return nil, errors.New("Monitor does not exist.")
+}
+
+
+func GetProvisionedMonitors(config conf.Config) ([]datadog.Monitor, error) {
+  client := getDDClient(config)
+  return client.GetMonitorsByTags([]string{config.OwnerTag})
+}
+
+
+func DeleteMonitor(config conf.Config, AlertId int) error {
+  client := getDDClient(config)
+  return client.DeleteMonitor(AlertId)
+}
+
+
+func UpdateMonitor(config conf.Config, monitor *datadog.Monitor) error {
+  //TODO - Update monitor
+  return nil
+}
+
+
+func getDDClient(config conf.Config) *datadog.Client {
+  client := datadog.NewClient(config.DatadogApiKey, config.DatadogAppKey)
+  return client
 }
