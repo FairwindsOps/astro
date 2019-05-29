249f851b9c15ef81398fa39137bbc137849ae312
Bobby Brennan <bobby.brennan@gmail.com>
2019-05-29 13:23:41 +0000

    Run golint and go fmt

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/reactiveops/dd-manager/cmd"
 
 func main() {
-  cmd.RootCmd().Execute()
+	cmd.RootCmd().Execute()
 }
--- pkg/handler/deployments.go
+++ pkg/handler/deployments.go
@@ -14,37 +14,36 @@
 
 package handler
 
-
 import (
+	"fmt"
+	"github.com/reactiveops/dd-manager/pkg/config"
+	"github.com/reactiveops/dd-manager/pkg/util"
 	log "github.com/sirupsen/logrus"
 	appsv1 "k8s.io/api/apps/v1"
-  "github.com/reactiveops/dd-manager/pkg/config"
-  "github.com/reactiveops/dd-manager/pkg/util"
-  "strings"
-  "fmt"
+	"strings"
 )
 
 // OnDeploymentChanged is a handler that should be called when a deployment chanages.
 func OnDeploymentChanged(deployment *appsv1.Deployment, event config.Event) {
-  cfg := config.New()
+	cfg := config.New()
 
-  switch strings.ToLower(event.EventType) {
-  case "delete":
-    if cfg.DryRun == false {
-      log.Info("Deleting resource monitors.")
-      util.DeleteMonitors([]string{cfg.OwnerTag, fmt.Sprintf("dd-manager:object_type:%s", event.ResourceType), fmt.Sprintf("dd-manager:resource:%s", event.Key)})
-    }
-  case "create", "update":
-    var monitors []config.Monitor
-    monitors = append(*cfg.GetMatchingMonitors(deployment.Annotations, event.ResourceType), *cfg.GetBoundMonitors(event.Namespace, event.ResourceType)...)
-    for _, monitor := range monitors {
-      log.Infof("Reconcile monitor %s", monitor.Name)
-      applyTemplate(deployment, &monitor, &event)
-      if cfg.DryRun == false {
-        util.AddOrUpdate(&monitor)
-      }
-    }
-  default:
-    log.Warnf("Update type %s is not valid, skipping.", event.EventType)
-  }
+	switch strings.ToLower(event.EventType) {
+	case "delete":
+		if cfg.DryRun == false {
+			log.Info("Deleting resource monitors.")
+			util.DeleteMonitors([]string{cfg.OwnerTag, fmt.Sprintf("dd-manager:object_type:%s", event.ResourceType), fmt.Sprintf("dd-manager:resource:%s", event.Key)})
+		}
+	case "create", "update":
+		var monitors []config.Monitor
+		monitors = append(*cfg.GetMatchingMonitors(deployment.Annotations, event.ResourceType), *cfg.GetBoundMonitors(event.Namespace, event.ResourceType)...)
+		for _, monitor := range monitors {
+			log.Infof("Reconcile monitor %s", monitor.Name)
+			applyTemplate(deployment, &monitor, &event)
+			if cfg.DryRun == false {
+				util.AddOrUpdate(&monitor)
+			}
+		}
+	default:
+		log.Warnf("Update type %s is not valid, skipping.", event.EventType)
+	}
 }
