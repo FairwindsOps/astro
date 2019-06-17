f100c6176bd66071092ea7106f5ec0bf3e85c9b8
Bobby Brennan <bobby.brennan@gmail.com>
2019-06-17 13:43:51 +0000

    Use controller-runtime to resolve kubeconfig

--- pkg/kube/client.go
+++ pkg/kube/client.go
@@ -1,13 +1,9 @@
 package kube
 
 import (
-	homedir "github.com/mitchellh/go-homedir"
 	log "github.com/sirupsen/logrus"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
-	"os"
-	"path/filepath"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sync"
 )
 
@@ -37,45 +33,13 @@ func SetInstance(kc ClientInstance) {
 }
 
 func getKubeClient() kubernetes.Interface {
-	config, err := rest.InClusterConfig()
+	kubeConf, err := config.GetConfig()
 	if err != nil {
-		// not in cluster
-		kubeconfig := getKubeConfigPath()
-		localConfig, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-		if err != nil {
-			panic(err)
-		}
-		clientset, err := kubernetes.NewForConfig(localConfig)
-		if err != nil {
-			panic(err)
-		}
-		return clientset
+		log.Fatalf("Error getting kubeconfig: %v", err)
 	}
-	// in cluster
-	clientset, err := kubernetes.NewForConfig(config)
+	clientset, err := kubernetes.NewForConfig(kubeConf)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Error creating kubernetes client: %v", err)
 	}
 	return clientset
-}
-
-// getKubeConfigPath returns a valid kubeconfig path.
-func getKubeConfigPath() string {
-	var path string
-
-	if os.Getenv("KUBECONFIG") != "" {
-		path = os.Getenv("KUBECONFIG")
-	} else if home, err := homedir.Dir(); err == nil {
-		path = filepath.Join(home, ".kube", "config")
-	} else {
-		log.Fatal("kubeconfig not found.  Please ensure ~/.kube/config exists or KUBECONFIG is set.")
-		os.Exit(1)
-	}
-
-	// kubeconfig doesn't exist
-	if _, err := os.Stat(path); err != nil {
-		log.Fatalf("%s doesn't exist - do you have a kubeconfig configured?\n", path)
-		os.Exit(1)
-	}
-	return path
 }
