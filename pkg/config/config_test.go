45b6184bd5c337645714b6e0ea05b1f58b65eec2
Luke Reed <luke@reactiveops.com>
2019-07-15 10:33:02 -0400

    unit-testing for config package + a few small fixes elswhere (#18)
    * spelling fixes noted while working on tests
    * fixing logic of appending tags in config pkg. it was not working before
    * adding tests for pkg/config and a single test for pkg/controller
    * renaming README and also adding the proper badge link with fairwinds rename
    * copy/paste error in .circleci/build.config
    * moving the config package test yml to a better name to avoid confusion
    * adding proper codecov badge to README - fairwindsops instead of reactiveops

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -1,55 +1,175 @@
 package config
 
 import (
+	"bytes"
+	"os"
 	"testing"
 
+	"github.com/reactiveops/dd-manager/pkg/kube"
+	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
 )
 
-func TestGetDeploymentRulesets(t *testing.T) {
-	cfg := &Config{
-		MonitorDefinitionsPath: "../../conf.yml",
-	}
-	cfg.reloadRulesets()
-
-	annotations := make(map[string]string, 3)
-	annotations["dd-manager/owner"] = "dd-manager"
-	annotations["app"] = "foo"
-	annotations["bar"] = "baz"
-
-	objectType := "deployment"
-
-	mSets := cfg.getMatchingRulesets(annotations, objectType)
-	assert.Equal(t, 1, len(*mSets))
-	mSet := (*mSets)[0]
-	assert.Equal(t, objectType, mSet.ObjectType)
-	assert.Equal(t, 1, len(mSet.Monitors))
-	assert.Equal(t, "Deployment Replica Alert - {{ .ObjectMeta.Name }}", *mSet.Monitors[0].Name)
-
-	monitors := cfg.GetMatchingMonitors(annotations, objectType)
-	assert.Equal(t, mSet.Monitors, *monitors)
+var annotationCases = map[string]map[string]string{
+	"pass": {
+		"dd-manager/owner": "dd-manager",
+	},
+	"fail": {
+		"test": "fail",
+	},
 }
 
-func TestGetNamespaceRulesets(t *testing.T) {
-	cfg := &Config{
-		MonitorDefinitionsPath: "../../conf.yml",
+var typeCases = map[string]map[string]string{
+	"deployment": {
+		"title": "Deployment Replica Alert - {{ .ObjectMeta.Name }}",
+	},
+	"namespace": {
+		"title": "Namespaced Deployment Replica Alert - {{ .ObjectMeta.Name }}",
+	},
+}
+
+func mockKube() *kube.ClientInstance {
+	kubeClient := kube.ClientInstance{
+		Client: fake.NewSimpleClientset(),
 	}
-	cfg.reloadRulesets()
+	kube.SetInstance(kubeClient)
+	return &kubeClient
+}
 
-	annotations := make(map[string]string, 3)
-	annotations["dd-manager/owner"] = "dd-manager"
-	annotations["app"] = "foo"
-	annotations["bar"] = "baz"
+func getConf(confPath string) *Config {
+	config := &Config{
+		MonitorDefinitionsPath: confPath,
+	}
+	config.reloadRulesets()
+	return config
+}
 
-	objectType := "namespace"
+var cfg = getConf("./test_conf.yml")
 
-	mSets := cfg.getMatchingRulesets(annotations, objectType)
+// Because we use sync.Once.Do() to load the config it's difficult to test the logic when DD_API_KEY or DD_APP_KEY env vars are not present
+func TestGetInstance(t *testing.T) {
+	os.Setenv("DD_API_KEY", "dummy")
+	os.Setenv("DD_APP_KEY", "dummy")
+	os.Setenv("CLUSTER_NAME", "dummy")
+	os.Setenv("OWNER", "dummy")
+	os.Setenv("DEFINITIONS_PATH", "./test_conf.yml")
+	os.Setenv("DRY_RUN", "false")
+
+	cfg := GetInstance()
+	assert.Equal(t, "dummy", (*cfg).DatadogAPIKey)
+	assert.Equal(t, "dummy", (*cfg).DatadogAppKey)
+	assert.Equal(t, "dummy", (*cfg).ClusterName)
+	assert.Equal(t, "dummy", (*cfg).OwnerTag)
+	assert.Equal(t, false, (*cfg).DryRun)
+	assert.Equal(t, "./test_conf.yml", (*cfg).MonitorDefinitionsPath)
+}
+
+func TestGetRulesetsValid(t *testing.T) {
+	for objectType, items := range typeCases {
+		annotations := annotationCases["pass"]
+		mSets := cfg.getMatchingRulesets(annotations, objectType)
+		assert.Equal(t, 1, len(*mSets))
+		mSet := (*mSets)[0]
+		assert.Equal(t, objectType, mSet.ObjectType)
+		assert.Equal(t, 1, len(mSet.Monitors))
+		assert.Equal(t, items["title"], *mSet.Monitors[0].Name)
+
+		monitors := cfg.GetMatchingMonitors(annotations, objectType)
+		assert.Equal(t, mSet.Monitors, *monitors)
+	}
+}
+
+func TestGetRulesetsInvalid(t *testing.T) {
+	for objectType := range typeCases {
+		annotations := annotationCases["fail"]
+		mSets := cfg.getMatchingRulesets(annotations, objectType)
+		assert.Equal(t, 0, len(*mSets))
+
+	}
+}
+
+func TestGetBoundMonitorsValid(t *testing.T) {
+	kubeClient := mockKube()
+	annotations := make(map[string]string, 1)
+	annotations["test"] = "yup"
+
+	ns := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "owned-namespace",
+			Annotations: annotations,
+		},
+	}
+	kubeClient.Client.CoreV1().Namespaces().Create(ns)
+
+	dep := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "foo",
+			Annotations: annotations,
+		},
+	}
+	kubeClient.Client.AppsV1().Deployments("foo").Create(dep)
+
+	mSets := cfg.GetBoundMonitors("owned-namespace", "deployment")
 	assert.Equal(t, 1, len(*mSets))
-	mSet := (*mSets)[0]
-	assert.Equal(t, objectType, mSet.ObjectType)
-	assert.Equal(t, 1, len(mSet.Monitors))
-	assert.Equal(t, "Namespaced Deployment Replica Alert - {{ .ObjectMeta.Name }}", *mSet.Monitors[0].Name)
+	assert.Contains(t, (*mSets)[0].Tags, "dd-manager:bound_object")
+}
 
-	monitors := cfg.GetMatchingMonitors(annotations, objectType)
-	assert.Equal(t, mSet.Monitors, *monitors)
+func TestGetBoundMonitorsInvalid(t *testing.T) {
+	kubeClient := mockKube()
+	annotations := make(map[string]string, 1)
+	annotations["test"] = "nope"
+	ns := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "owned-namespace",
+			Annotations: annotations,
+		},
+	}
+	kubeClient.Client.CoreV1().Namespaces().Create(ns)
+
+	mSets := cfg.GetBoundMonitors("owned-namespace", "deployment")
+	assert.Equal(t, 0, len(*mSets))
+
+	deleteOptions := metav1.NewDeleteOptions(10)
+	kubeClient.Client.CoreV1().Namespaces().Delete("owned-namespace", deleteOptions)
+
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	defer func() {
+		log.SetOutput(os.Stdout)
+	}()
+	cfg.GetBoundMonitors("owned-namespace", "deployment")
+	assert.Contains(t, buf.String(), "Error getting namespace")
+}
+
+func TestGenEnvAsInt(t *testing.T) {
+	os.Setenv("testing", "1")
+	presentEnv := envAsInt("testing", 0)
+	notPresentEnv := envAsInt("testing1", 0)
+	assert.Equal(t, 1, presentEnv)
+	assert.Equal(t, 0, notPresentEnv)
+}
+
+func TestGetEnvAsBool(t *testing.T) {
+	os.Setenv("present", "true")
+	presentEnv := envAsBool("present", false)
+	notPresentEnv := envAsBool("notpresent", false)
+	assert.Equal(t, true, presentEnv)
+	assert.Equal(t, false, notPresentEnv)
+}
+
+func TestLoadFromPathInvalid(t *testing.T) {
+	var invalidHTTP = "http://fake.fake/config.yml"
+	var invalidLocal = "./fake.yml"
+
+	data, err := loadFromPath(invalidHTTP)
+	assert.Error(t, err)
+	assert.Empty(t, data)
+
+	data, err = loadFromPath(invalidLocal)
+	assert.Error(t, err)
+	assert.Empty(t, data)
 }
