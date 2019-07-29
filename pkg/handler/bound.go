187b42d0264a2bab76688e66018da77579f52e82
Luke Reed <luke@lreed.net>
2019-07-29 13:43:37 -0700

    cleaning up code a bit so it can more easily support other bound resources in the future

--- pkg/handler/bound.go
+++ pkg/handler/bound.go
@@ -20,6 +20,7 @@ import (
 	"github.com/fairwindsops/dd-manager/pkg/config"
 	"github.com/fairwindsops/dd-manager/pkg/kube"
 	log "github.com/sirupsen/logrus"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -32,15 +33,21 @@ func updateBoundResources(namespace *corev1.Namespace) {
 		return
 	}
 	for _, dep := range deploys.Items {
-		var evt config.Event
-		evt.Key = fmt.Sprintf("%s/%s", namespace.Name, dep.Name)
-		if err != nil {
-			log.Errorf("Error handling bound deployment update event")
-			return
-		}
-		evt.EventType = "update"
-		evt.ResourceType = "deployment"
-		evt.Namespace = namespace.Name
+		evt := setupBoundEvent(&dep)
 		OnDeploymentChanged(&dep, evt)
 	}
+}
+
+func setupBoundEvent(obj interface{}) config.Event {
+	var evt config.Event
+	switch object := obj.(type) {
+	case *appsv1.Deployment:
+		evt.Key = fmt.Sprintf("%s/%s", object.Namespace, object.Name)
+		evt.Namespace = object.Namespace
+		evt.ResourceType = "deployment"
+	default:
+		log.Warnf("Object has unknown type of %T", object)
+	}
+	evt.EventType = "update"
+	return evt
 }
