d85dc604ce20b8403ff8bc5c57405eb67ee62f72
Luke Reed <luke@lreed.net>
2019-08-05 11:18:43 -0400

    adding namespace creation that is expected in handler test

--- pkg/handler/deployments_test.go
+++ pkg/handler/deployments_test.go
@@ -57,6 +57,13 @@ func TestDeploymentChangeNoMatch(t *testing.T) {
 	kubeClient := kube.SetAndGetMock()
 	defer ctrl.Finish()
 
+	ns := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "foo",
+		},
+	}
+	kubeClient.Client.CoreV1().Namespaces().Create(ns)
+
 	annotations := make(map[string]string, 1)
 	annotations["dd-manager/owner"] = "not-dd-manager"
 	dep := &appsv1.Deployment{
