af17e1fbb23b07d56b39bbf4beba9869560d253d
Micah Huber <micah@fastmail.us>
2019-10-22 20:01:12 -0400

    Add e2e tests

---
+++ e2e/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+vertical_pod_autoscaler_tag=vertical-pod-autoscaler-0.5.1
+
+printf "\n\n"
+echo "**************************"
+echo "** Begin E2E Test Setup **"
+echo "**************************"
+printf "\n\n"
+
+set -e
+
+printf "\n\n"
+echo "********************************************************************"
+echo "** Install Astro at $CI_SHA1 **"
+echo "********************************************************************"
+printf "\n\n"
+
+kubectl create ns astro
+kubectl -n astro apply -f /hack/manifests/
+
+kubectl -n astro wait deployment --timeout=60s --for condition=available -l app.kubernetes.io/name=astro,app.kubernetes.io/instance=astro
+
+kubectl get po --all-namespaces
